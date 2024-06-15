c9d5383a069dfb921f4de13b7943d47db5fe0da0
ianahilario <iana.hilario@gmail.com>
2024-06-15 21:57:38 +0800

    add visual test support on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   maxFailures: process.env.CI ? 9 : undefined,
+  snapshotPathTemplate: '.visual-test-snapshots/{projectName}/{testFilePath}/{arg}{ext}',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
