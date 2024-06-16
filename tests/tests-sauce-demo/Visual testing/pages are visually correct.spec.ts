956c4373e29b560b3a35c10dc81f9a12886bd53e
ianahilario <iana.hilario@gmail.com>
2024-06-16 15:24:15 +0800

    set maxDiffPixelRatio: 0.2

--- tests/tests-sauce-demo/Visual testing/pages are visually correct.spec.ts
+++ tests/tests-sauce-demo/Visual testing/pages are visually correct.spec.ts
@@ -11,39 +11,39 @@ test('visual testing', {tag: '@visual'},  async ({page, loginPage, header, produ
 
     await test.step(`product listing page`, async () => {
         await productListingPage.isCorrectPage();
-        await expect.soft(page).toHaveScreenshot({fullPage : true});
+        await expect.soft(page).toHaveScreenshot({fullPage : true, maxDiffPixelRatio: 0.2});
     });
 
     await test.step(`product details page`, async () => {
         await productListingPage.goToDetailsPage();
         await productDetailsPage.isCorrectPage();
-        await expect.soft(page).toHaveScreenshot({fullPage : true});
+        await expect.soft(page).toHaveScreenshot({fullPage : true, maxDiffPixelRatio: 0.2});
         await productDetailsPage.cartItem.addItemToCartbyIndex();
     });
     
     await test.step(`cart page`, async () => {
         await header.goToShoppingCartPage();
         await cartPage.isCorrectPage();
-        await expect.soft(page).toHaveScreenshot({fullPage : true});
+        await expect.soft(page).toHaveScreenshot({fullPage : true, maxDiffPixelRatio: 0.2});
     });
 
     await test.step(`cart checkout page`, async () => {
         await cartPage.gotoCartCheckoutPage();
         await cartCheckoutPage.isCorrectPage();
-        await expect.soft(page).toHaveScreenshot({fullPage : true});
+        await expect.soft(page).toHaveScreenshot({fullPage : true, maxDiffPixelRatio: 0.2});
         await cartCheckoutPage.fillCheckoutForm();
     });
 
     await test.step(`cart review page`, async () => {
         await cartCheckoutPage.gotoCartReviewPage();
         await cartReviewPage.isCorrectPage();
-        await expect.soft(page).toHaveScreenshot({fullPage : true});
+        await expect.soft(page).toHaveScreenshot({fullPage : true, maxDiffPixelRatio: 0.2});
     });
 
     await test.step(`confirmation page`, async () => {
         await cartReviewPage.gotoCartConfirmationPage();
         await cartConfirmationPage.isCorrectPage();
-        await expect.soft(page).toHaveScreenshot({fullPage : false});
+        await expect.soft(page).toHaveScreenshot({fullPage : false, maxDiffPixelRatio: 0.2});
     });
 });
 
