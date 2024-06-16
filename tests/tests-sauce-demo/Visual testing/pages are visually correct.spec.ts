e820c6ea28c6f2b28f5a9411ddc9285beb84b479
ianahilario <iana.hilario@gmail.com>
2024-06-16 14:57:11 +0800

    update github actions

--- tests/tests-sauce-demo/Visual testing/pages are visually correct.spec.ts
+++ tests/tests-sauce-demo/Visual testing/pages are visually correct.spec.ts
@@ -5,19 +5,19 @@ import { test } from '../../../fixtures/testBase';
 test('visual testing', {tag: '@visual'},  async ({page, loginPage, header, productListingPage, productDetailsPage, cartPage, cartCheckoutPage, cartReviewPage, cartConfirmationPage }) => {
     await test.step(`login page`, async () => {
         await loginPage.goToLoginPage();
-        await expect(page).toHaveScreenshot();
+        await expect.soft(page).toHaveScreenshot({fullPage : true});
         await loginPage.submitLogin(`${process.env.USER_USERNAME}`, `${process.env.USER_PASSWORD}`);
     });
 
     await test.step(`product listing page`, async () => {
         await productListingPage.isCorrectPage();
-        await expect(page).toHaveScreenshot();
+        await expect.soft(page).toHaveScreenshot({fullPage : true});
     });
 
     await test.step(`product details page`, async () => {
         await productListingPage.goToDetailsPage();
         await productDetailsPage.isCorrectPage();
-        await expect(page).toHaveScreenshot();
+        await expect.soft(page).toHaveScreenshot({fullPage : true});
         await productDetailsPage.cartItem.addItemToCartbyIndex();
     });
     
@@ -30,20 +30,20 @@ test('visual testing', {tag: '@visual'},  async ({page, loginPage, header, produ
     await test.step(`cart checkout page`, async () => {
         await cartPage.gotoCartCheckoutPage();
         await cartCheckoutPage.isCorrectPage();
-        await expect(page).toHaveScreenshot();
+        await expect.soft(page).toHaveScreenshot({fullPage : true});
         await cartCheckoutPage.fillCheckoutForm();
     });
 
     await test.step(`cart review page`, async () => {
         await cartCheckoutPage.gotoCartReviewPage();
         await cartReviewPage.isCorrectPage();
-        await expect(page).toHaveScreenshot();
+        await expect.soft(page).toHaveScreenshot({fullPage : true});
     });
 
     await test.step(`confirmation page`, async () => {
         await cartReviewPage.gotoCartConfirmationPage();
         await cartConfirmationPage.isCorrectPage();
-        await expect(page).toHaveScreenshot();
+        await expect.soft(page).toHaveScreenshot({fullPage : true});
     });
 });
 
