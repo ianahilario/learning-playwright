c2ffd6afdcd376fa8af7677d60615b9e5a100dde
ianahilario <iana.hilario@gmail.com>
2024-09-29 10:15:12 +0800

    update es-lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,41 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ["**/**.{ts}"],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: '.',
+      },
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      'no-empty': 'warn',
+      'no-useless-escape': 'warn',
+      'no-empty-pattern': 'warn',
+      'comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          objects: 'always-multiline',
+          imports: 'never',
+          exports: 'never',
+          functions: 'never',
+        },
+      ],
+      "space-infix-ops": ["error"],
+      '@stylistic/js/indent': ['error', 2],
+      'object-curly-spacing': ['error', 'always'],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      '@typescript-eslint/await-thenable': 'warn',
+    },
+  },
 ];
