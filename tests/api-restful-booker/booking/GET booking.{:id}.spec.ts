6ee72817f0655bced20eb5c6610563a21adb59c2
ianahilario <iana.hilario@gmail.com>
2024-10-05 22:27:48 +0800

    update test

--- tests/api-restful-booker/booking/GET booking.{:id}.spec.ts
+++ tests/api-restful-booker/booking/GET booking.{:id}.spec.ts
@@ -10,23 +10,17 @@ test('able to get return booking details', async ({ request }) => {
   const responseBody = await response.json();
   console.log(responseBody);
 
-  expect.soft(responseBody).toHaveProperty('firstname', 'Michael');
-  expect.soft(responseBody).toHaveProperty('lastname', 'Pangilinan');
-  expect.soft(responseBody).toHaveProperty('totalprice', 100);
+  expect.soft(responseBody).toHaveProperty('firstname', 'Sally');
+  expect.soft(responseBody).toHaveProperty('lastname', 'Brown');
+  expect.soft(responseBody).toHaveProperty('totalprice', 290);
   expect.soft(responseBody).toHaveProperty('depositpaid', true);
-  expect
-    .soft(responseBody.bookingdates)
-    .toHaveProperty('checkin', '2023-06-01');
-  expect
-    .soft(responseBody.bookingdates)
-    .toHaveProperty('checkout', '2024-06-27');
-  expect.soft(responseBody).toHaveProperty('additionalneeds', 'with window');
+  expect.soft(responseBody.bookingdates).toHaveProperty('checkin', '2019-07-11');
+  expect.soft(responseBody.bookingdates).toHaveProperty('checkout', '2022-02-06');
+  expect.soft(responseBody).toHaveProperty('additionalneeds', 'Breakfast');
 });
 
 test('able to handle non-existing booking id', async ({ request }) => {
-  const response = await request.get(
-    `https://restful-booker.herokuapp.com/booking/abc`
-  );
+  const response = await request.get(`https://restful-booker.herokuapp.com/booking/abc`);
 
   expect(response.ok()).not.toBeTruthy();
   expect(response.status()).toBe(404);
