970eba7655ca985708c82ad83e3cc05a8bc6ea05
ianahilario <iana.hilario@gmail.com>
2024-11-09 22:50:24 +0800

    add tests for payment method checkout

--- models/practice-software-testing/cart-page.ts
+++ models/practice-software-testing/cart-page.ts
@@ -15,17 +15,53 @@ interface BankTransferForm {
   accountNumberField: Locator;
 }
 
+interface CreditCardForm {
+  cardNumberField: Locator;
+  expirationDateField: Locator;
+  cvvField: Locator;
+  cardholderNameField: Locator;
+}
+
+interface BuyNowPayLaterForm {
+  installmentDropdown: Locator;
+}
+
+interface GiftCardForm {
+  giftCardNumberField: Locator;
+  validationCodeField: Locator;
+}
+
+export enum PaymentMethodOptions {
+  BANK_TRANSFER = 'bank-transfer',
+  CASH_ON_DELIVERY = 'cash-on-delivery',
+  CREDIT_CARD = 'credit-card',
+  BUY_NOW_PAY_LATER = 'buy-now-pay-later',
+  GIFT_CARD = 'gift-card'
+}
+
+export enum BuyNowPayLaterOptions {
+  MONTHS_3 = '3',
+  MONTHS_6 = '6',
+  MONTHS_9 = '9',
+  MONTHS_12 = '12'
+}
+
 export class CartPage extends BasePage {
+  readonly cartItems: Locator;
   readonly proceedToCheckoutButton: Locator;
   readonly proceedToBillingButton: Locator;
   readonly proceedToPaymentButton: Locator;
   readonly billingAddressForm: BillingAddressForm;
   readonly paymentMethodDropdown: Locator;
   readonly bankTransferForm: BankTransferForm;
+  readonly creditCardForm: CreditCardForm;
+  readonly buyNowPayLaterForm: BuyNowPayLaterForm;
+  readonly giftCardForm: GiftCardForm;
   readonly confirmButton: Locator;
 
   constructor(page: Page, isMobile: boolean) {
     super(page, isMobile);
+    this.cartItems = this.page.locator('//tbody');
     this.proceedToCheckoutButton = this.page.locator(
       '//*[@data-test="proceed-1"]'
     );
@@ -49,6 +85,31 @@ export class CartPage extends BasePage {
       bankNameField: this.page.locator('//*[@data-test="bank_name"]'),
       accountNameField: this.page.locator('//*[@data-test="account_name"]'),
       accountNumberField: this.page.locator('//*[@data-test="account_number"]')
+    };
+    this.creditCardForm = {
+      cardNumberField: this.page.locator(
+        '//*[@data-test="credit_card_number"]'
+      ),
+      expirationDateField: this.page.locator(
+        '//*[@data-test="expiration_date"]'
+      ),
+      cvvField: this.page.locator('//*[@data-test="cvv"]'),
+      cardholderNameField: this.page.locator(
+        '//*[@data-test="card_holder_name"]'
+      )
+    };
+    this.buyNowPayLaterForm = {
+      installmentDropdown: this.page.locator(
+        '//*[@data-test="monthly_installments"]'
+      )
+    };
+    this.giftCardForm = {
+      giftCardNumberField: this.page.locator(
+        '//*[@data-test="gift_card_number"]'
+      ),
+      validationCodeField: this.page.locator(
+        '//*[@data-test="validation_code"]'
+      )
     };
     this.confirmButton = this.page.locator('//*[@data-test="finish"]');
   }
@@ -83,17 +144,6 @@ export class CartPage extends BasePage {
       )
       .getByText('Payment')
       .waitFor({ timeout: 5_000 });
-  }
-
-  async selectPaymentMethod(
-    paymentMethod:
-      | 'bank-transfer'
-      | 'cash-on-delivery'
-      | 'credit-card'
-      | 'buy-now-pay-later'
-      | 'gift-card'
-  ) {
-    await this.paymentMethodDropdown.selectOption(paymentMethod);
   }
 
   async gotoOrderConfirmationPage() {
--- tests/practice-software-testing/customer/checkout-by-payment-method.spec.ts
+++ tests/practice-software-testing/customer/checkout-by-payment-method.spec.ts
@@ -1,9 +1,12 @@
 import { expect } from '@playwright/test';
 import { test } from '../../../fixtures/practice-software-testing';
 import { faker } from '@faker-js/faker';
+import {
+  BuyNowPayLaterOptions,
+  CartPage,
+  PaymentMethodOptions
+} from '../../../models/practice-software-testing/cart-page';
 
-let cartQuantity = 0;
-let invoiceNumber: string;
 test.beforeEach(async ({ navigation, loginPage }) => {
   await test.step(`login`, async () => {
     await navigation.gotoHomepageviaURL();
@@ -13,78 +16,94 @@ test.beforeEach(async ({ navigation, loginPage }) => {
   });
 });
 
-test(
-  'checkout shopping cart via bank transfer',
-  { tag: '@smoke' },
-  async ({ navigation, homepage, productDetailsPage, cartPage, page }) => {
-    await test.step(`should have an empty cart item`, async () => {
-      expect(
-        await navigation.header.getCartQuantity(),
-        'Cart quantity indicator is not displayed'
-      ).toBe(cartQuantity);
-    });
+let invoiceNumber: string;
 
-    await test.step(`add an item to cart`, async () => {
-      await homepage.productListing.gotoProductDetailsPage();
-      await productDetailsPage.addItemToCart();
-      await expect(productDetailsPage.toastMessage).toBeVisible();
-      await expect
-        .soft(productDetailsPage.toastMessage)
-        .toHaveText('Product added to shopping cart.');
-      cartQuantity++;
-      expect(
-        await navigation.header.getCartQuantity(),
-        `Cart quantity is ${cartQuantity}`
-      ).toBe(cartQuantity);
-    });
+for (const paymentMethod of Object.values(PaymentMethodOptions)) {
+  test(
+    `checkout shopping cart via ${paymentMethod}`,
+    { tag: '@smoke' },
+    async ({ navigation, homepage, productDetailsPage, cartPage, page }) => {
+      await test.step(`add an item to cart`, async () => {
+        await homepage.productListing.gotoProductDetailsPage();
+        await productDetailsPage.addItemToCart();
+        await expect(productDetailsPage.toastMessage).toBeVisible();
+      });
 
-    await test.step(`proceed to checkout`, async () => {
-      await navigation.header.gotoCartPage();
-      await cartPage.gotoSignInStep();
-      await cartPage.gotoBillingAddressStep();
-      await cartPage.billingAddressForm.addressField.fill(
-        faker.location.streetAddress()
-      );
-      await cartPage.billingAddressForm.cityField.fill(faker.location.city());
-      await cartPage.billingAddressForm.stateField.fill(faker.location.state());
-      await cartPage.billingAddressForm.countryField.fill(
-        faker.location.country()
-      );
-      await cartPage.billingAddressForm.postodeField.fill(
-        faker.number.int({ min: 10000, max: 99999 }).toString()
-      );
-    });
+      await test.step(`proceed to checkout`, async () => {
+        await navigation.header.gotoCartPage();
+        await cartPage.gotoSignInStep();
+        await cartPage.gotoBillingAddressStep();
+        await cartPage.billingAddressForm.addressField.fill(
+          faker.location.streetAddress()
+        );
+        await cartPage.billingAddressForm.cityField.fill(faker.location.city());
+        await cartPage.billingAddressForm.stateField.fill(
+          faker.location.state()
+        );
+        await cartPage.billingAddressForm.countryField.fill(
+          faker.location.country()
+        );
+        await cartPage.billingAddressForm.postodeField.fill(
+          faker.number.int({ min: 10000, max: 99999 }).toString()
+        );
+      });
 
-    await test.step(`select payment option and submit`, async () => {
-      await cartPage.gotoPaymentStep();
-      await cartPage.paymentMethodDropdown.selectOption('bank-transfer');
-      await cartPage.bankTransferForm.bankNameField.fill('Bank XYZ');
-      await cartPage.bankTransferForm.accountNameField.fill(
-        faker.finance.accountName()
-      );
-      await cartPage.bankTransferForm.accountNumberField.fill(
-        faker.finance.accountNumber()
-      );
-    });
+      await test.step(`select payment option and submit`, async () => {
+        await cartPage.gotoPaymentStep();
+        await fillPaymentForm(cartPage, paymentMethod);
+      });
 
-    await test.step(`confirm the order`, async () => {
-      await cartPage.gotoOrderConfirmationPage();
-      invoiceNumber = await cartPage.getOrderInvoice();
-      cartQuantity--;
-      expect
-        .soft(
-          await navigation.header.getCartQuantity(),
-          `Cart quantity is ${cartQuantity}`
-        )
-        .toBe(cartQuantity);
-    });
+      await test.step(`confirm the order`, async () => {
+        await cartPage.gotoOrderConfirmationPage();
+        invoiceNumber = await cartPage.getOrderInvoice();
+      });
 
-    await test.step(`should show generated invoice in user's invoice list`, async () => {
-      await navigation.header.gotoUserMenu('my-invoices');
-      await expect(
-        page.getByText(invoiceNumber),
-        "Invoice is displayed in user's invoice list"
-      ).toBeVisible();
-    });
+      await test.step(`should show generated invoice in user's invoice list`, async () => {
+        await navigation.header.gotoUserMenu('my-invoices');
+        await expect(
+          page.getByText(invoiceNumber),
+          "Invoice is displayed in user's invoice list"
+        ).toBeVisible();
+      });
+    }
+  );
+}
+
+async function fillPaymentForm(
+  cartPage: CartPage,
+  paymentMethod: PaymentMethodOptions
+) {
+  await cartPage.paymentMethodDropdown.selectOption(paymentMethod);
+
+  if (paymentMethod === PaymentMethodOptions.BANK_TRANSFER) {
+    await cartPage.bankTransferForm.bankNameField.fill('Bank XYZ');
+    await cartPage.bankTransferForm.accountNameField.fill(
+      faker.finance.accountName()
+    );
+    await cartPage.bankTransferForm.accountNumberField.fill(
+      faker.finance.accountNumber()
+    );
   }
-);
+
+  if (paymentMethod === PaymentMethodOptions.CREDIT_CARD) {
+    await cartPage.creditCardForm.cardNumberField.fill('4242-4242-4242-4242');
+    await cartPage.creditCardForm.expirationDateField.fill('12/2999');
+    await cartPage.creditCardForm.cvvField.fill('123');
+    await cartPage.creditCardForm.cardholderNameField.fill('Tommy Clover');
+  }
+
+  if (paymentMethod === PaymentMethodOptions.BUY_NOW_PAY_LATER) {
+    await cartPage.buyNowPayLaterForm.installmentDropdown.selectOption(
+      BuyNowPayLaterOptions.MONTHS_6
+    );
+  }
+
+  if (paymentMethod === PaymentMethodOptions.GIFT_CARD) {
+    await cartPage.giftCardForm.giftCardNumberField.fill(
+      faker.string.alphanumeric({ length: 16 })
+    );
+    await cartPage.giftCardForm.validationCodeField.fill(
+      faker.string.alphanumeric({ length: 8 })
+    );
+  }
+}
