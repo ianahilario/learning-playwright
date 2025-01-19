6d8454b724b6a593b8f6f3bd510b89a1e5026f58
ianahilario <iana.hilario@gmail.com>
2025-01-19 14:55:33 +0800

    add sample monkey test

---
+++ tests/practice-software-testing/_misc/monkey-testing.spec.ts
@@ -0,0 +1,117 @@
+import { expect } from '@playwright/test';
+import { test } from '../../../fixtures/practice-software-testing';
+
+const errors: [string, string][] = [];
+test.describe('monkey testing', () => {
+  test.beforeEach(async ({ page }) => {
+    errors.length = 0;
+
+    page.on('pageerror', (err) => {
+      console.log('monkey test error: pageerror', err.message);
+      errors.push(['pageerror', err.message]);
+    });
+
+    page.on('requestfailed', (err) => {
+      if (!err.url().includes('practice')) return;
+      if (err.url().includes('google')) return;
+      errors.push([
+        'requestfailed',
+        `${err.method()} ${err.url()}: ${err.failure()?.errorText}`
+      ]);
+    });
+
+    // Load gremlins.js library
+    await page.addInitScript({
+      path: './node_modules/gremlins.js/dist/gremlins.min.js'
+    });
+  });
+
+  test.afterEach(async ({}) => {
+    let count = 1;
+    errors.forEach((error) => {
+      console.log(`monkey test error #${count}:`, error.toString());
+      count++;
+    });
+  });
+
+  test('homepage', async ({ page, navigation }) => {
+    // Navigate to the test page
+    await navigation.gotoHomepageviaURL();
+
+    // Execute chaos testing with a 30-second timeout
+    await page.evaluate(() => {
+      const gremlins = (window as any).gremlins;
+      return Promise.race([
+        new Promise((resolve) => {
+          gremlins
+            .createHorde({
+              randomizer: new gremlins.Chance(1234),
+              strategies: [gremlins.strategies.allTogether({ nb: 10000 })],
+              species: [
+                gremlins.species.clicker({
+                  clickTypes: ['click'],
+                  canClick: function (element) {
+                    if (element.tagName === 'a') return false;
+                    if (element.classList.contains('figure-caption'))
+                      return false;
+                  }
+                }),
+                gremlins.species.formFiller(),
+                gremlins.species.typer(),
+                gremlins.species.scroller()
+              ],
+              mogwais: [
+                gremlins.mogwais.alert(),
+                gremlins.mogwais.fps(),
+                gremlins.mogwais.gizmo()
+              ],
+              delay: 250
+            })
+            .unleash();
+        }),
+        new Promise((resolve) => setTimeout(resolve, 30000)) // 30 seconds timeout
+      ]);
+    });
+
+    expect(errors.length, 'There are 0 errors in the page').toBe(0);
+  });
+
+  test('sign-in page', async ({ page, navigation }) => {
+    await navigation.gotoHomepageviaURL();
+    await navigation.header.gotoLogin();
+
+    await page.evaluate(() => {
+      const gremlins = (window as any).gremlins;
+      return Promise.race([
+        new Promise((resolve) => {
+          gremlins
+            .createHorde({
+              randomizer: new gremlins.Chance(1234),
+              strategies: [gremlins.strategies.allTogether({ nb: 10000 })],
+              species: [
+                gremlins.species.clicker({
+                  clickTypes: ['click'],
+                  canClick: function (element) {
+                    if (element.tagName === 'a') return false;
+                  }
+                }),
+                gremlins.species.formFiller(),
+                gremlins.species.typer(),
+                gremlins.species.scroller()
+              ],
+              mogwais: [
+                gremlins.mogwais.alert(),
+                gremlins.mogwais.fps(),
+                gremlins.mogwais.gizmo()
+              ],
+              delay: 250
+            })
+            .unleash();
+        }),
+        new Promise((resolve) => setTimeout(resolve, 30000)) // 30 seconds timeout
+      ]);
+    });
+
+    expect(errors.length, 'There are 0 errors in the page').toBe(0);
+  });
+});
